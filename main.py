d5b61707ffff59eb773c857561f198a8188073fc
Shaghayegh Ghasemi <shaghayegh5ghasemi@gmail.com>
2021-11-26 14:46:30 +0330

    preprocess func

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import openpyxl
 from pathlib import Path
-from parsivar import Normalizer
-from parsivar import Tokenizer
+from hazm import *
+
+class Document():
+    def __init__(self, id, content, title):
+        self.id = id
+        self.content = content
+        self.title = title
+    
 
 # this function collects the documents info from the excel file
 def get_documents():
@@ -12,17 +18,30 @@ def get_documents():
     title = doc_sheet['C']
     documents = [] #extract contents and their title from .xlsx file
     for i in range(1, len(content)):
-        documents.append([content[i].value, title[i].value])
+        documents.append(Document(i, content[i].value, title[i].value))
     return documents
 
-# this function prepossesses the documents: tokenize, normalize, stemming, delete stop words
-def preprocess():
+# this function prepossesses the document: tokenize, normalize, stemming, delete stop words
+def preprocess(raw_document):
     normalizer = Normalizer()
-    tokenizer = Tokenizer()
+    stemmer = Stemmer()
+    lemmatizer = Lemmatizer()
+    stopwordslist = set(stopwords_list())
+    tokens = []
+    words = word_tokenize(normalizer.normalize(raw_document.content))
+    for j in range(len(words)):
+        if (words[j] not in stopwordslist):
+            lemmatizer.lemmatize(words[j])
+            stemmer.stem(words[j])
+            # my tokens consists of: term, docID, position in the document
+            tokens.append([words[j], raw_document.id])
+    return tokens
 
-def positional_index():
+def positional_index(doc_collection):
     pass
 
 if __name__ == '__main__':
     docs = get_documents()
-    print(docs[0][1])
+    tokens = preprocess(docs[10])
+    
+
